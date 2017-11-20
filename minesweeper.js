8f8759de7efb07f2c89bcafdc25815ae9b0633c6
deadqruiser <foxgit@bloq.email>
2017-11-19 19:11:56 -0600

    first commit

---
+++ minesweeper.js
@@ -0,0 +1,5 @@
+const blankline = ' | | ';
+console.log('This is what an empty board would look like');
+console.log(blankline); console.log(blankline);
+console.log(blankline); console.log(blankline);
+console.log(blankline); console.log(blankline);
